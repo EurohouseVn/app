805da23568a16a70e5903537aeb44e41b3efe92e
EurohouseVn <eurohousethanhhoa@gmail.com>
2026-06-25 21:36:18 +0700

    fix api cors for vercel admin

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -13,7 +13,13 @@ function getCorsOrigins() {
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  app.enableCors({ origin: getCorsOrigins(), credentials: true });
+  app.enableCors({
+  origin: [
+    'https://app-admin-flame.vercel.app',
+    'https://app-admin-45xyvhamj-eurohouse.vercel.app',
+  ],
+  credentials: true,
+});
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
   const port = Number(process.env.PORT ?? 3001);
